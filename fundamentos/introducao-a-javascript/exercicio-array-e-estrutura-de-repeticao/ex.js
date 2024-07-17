11fe5736760e8b474e122119c2c9745ab5c77199
ALarissaLima <larissalima622@gmail.com>
2024-07-17 20:04:41 +0100

    Atualiza pastas front-end e back-end

---
+++ fundamentos/introducao-a-javascript/exercicio-array-e-estrutura-de-repeticao/ex.js
@@ -0,0 +1,8 @@
+let n = 5;
+let asteriscos = '*';
+for (let i = 0; i < n; ++i) {
+    asteriscos += '*';
+}
+for (let j = 0; j < n; ++j) {
+    console.log(asteriscos);
+}
---
+++ fundamentos/metodos-de-array/dia-03-metodos-de-array-find-e-foreach/exercicio-forach.js
@@ -0,0 +1,21 @@
+const arrayNum = [1,2,3,4,5,6,7,8,9,10];
+
+/* PARA IMPRIMIR INDEX E VALOR */
+arrayNum.forEach(printItem);
+
+function printItem(item, index, array){
+ console.log(index + '=' + item);
+}
+
+/* PARA IMPRIMIR VALOR E QUANTIDADE */
+
+letters = ['a','b','c','g','g','h','a'];
+
+let count = {};
+
+letters.forEach(item => {
+
+    
+}); 
+
+ 
